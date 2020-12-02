8b2c4a781fa46d6351d0abd1ba6207ec5216f1ce
fabsts <fabi.santos@edu.pbh.gov.br>
2020-12-01 23:36:06 -0300

    Reordenando arquivos

---
+++ src/views/Login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <!--  emmet html:5  cria a estrutura do html-->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Im'noutDocument</title>
+    <link rel="stylesheet" href="assets/css/comum.css"><!--  emmet link:css-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"><!--  emmet link:css-->
+    <link rel="stylesheet" href="assets/css/icofont.min.css"><!--  emmet link:css-->
+    <link rel="stylesheet" href="assets/css/login.css"><!--  emmet link:css-->
+</head>
+
+<body>
+<div>
+
+
+</div>
+</body>
+
+</html>
